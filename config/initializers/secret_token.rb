a602838386d69307530e6a209ce54f026b7e46b9
James Khatiblou <jkhatiblou@gmail.com>
2013-03-04 13:39:24 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JamesmkCom::Application.config.secret_token = '0a8f018a1d375f5acf4408b706a25a38866b88892abe5dcec2cd3cfb0abbed43fae1150808f9d833d499d1c05922fa4d68c3f9b6604c285d0f3d64e0931533a5'
