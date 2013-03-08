637f489c5f97cda9e6ae306e1916fb31bea933e7
James Khatiblou <jkhatiblou@gmail.com>
2013-03-08 14:14:28 -0800

    first commit of the day. Style changes to impress daddy

---
+++ public/assets/plugins/gadgetFonts.js
@@ -0,0 +1,7 @@
+/**
+ * GO GO GADGET GOOGLE FONTS
+ *
+ * @author Jim Ramsden me@jimram.com
+ * @copyright 2013 - Jim Ramsden
+ */
+WebFontConfig={google:{families:["Dosis:400,700:latin","Bitter:400,400italic,700:latin"]}},function(){var e=document.createElement("script");e.src=("https:"==document.location.protocol?"https":"http")+"://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js",e.type="text/javascript",e.async="true";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();
